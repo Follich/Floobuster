81c96e13671ce6998daaf04e3e3b53642eb43f7d
Old-Mistake <wfollt@gmail.com>
2024-07-03 16:44:37 -0300

    .

---
+++ floobuster.py
@@ -0,0 +1,46 @@
+import requests
+import threading
+
+
+def rd(url, txt, ends):
+    print("Executando...")
+    for line in txt:
+        response = requests.get(host + line)
+        if ends == "y":
+            print(f"{response.status_code} | GET | {url}{line}")
+        else:
+            if response.status_code < 404:
+                print(f"{response.status_code} | GET | {url}{line}")
+            else:
+                pass
+
+
+banner = """
+▄████  █    ████▄ ████▄ ███     ▄      ▄▄▄▄▄      ▄▄▄▄▀ ▄███▄   █▄▄▄▄
+█▀   ▀ █    █   █ █   █ █  █     █    █     ▀▄ ▀▀▀ █    █▀   ▀  █  ▄▀
+█▀▀    █    █   █ █   █ █ ▀ ▄ █   █ ▄  ▀▀▀▀▄       █    ██▄▄    █▀▀▌ 
+█      ███▄ ▀████ ▀████ █  ▄▀ █   █  ▀▄▄▄▄▀       █     █▄   ▄▀ █  █ 
+ █         ▀            ███   █▄ ▄█              ▀      ▀███▀     █  
+  ▀                            ▀▀▀                               ▀                                          
+"""
+print(banner)
+
+host = str(input("Qual o dominio? "))
+print("""
+1 - word01.txt
+2 - /usr/share/dirb/wordlists 
+3 - /usr/share/dirb/common.txt 
+4 - /usr/share/dirbuster/wordlists/directory-list-2.3-small.txt 
+5 - <caminho>
+
+""")
+
+word = str(input("Qual a wordlist? "))
+errors = str(input("Deseja que erros sejam exibidos?[y/n]")).lower().strip()
+pn = open(f"{word}", "rt")
+
+rd(host, pn, errors)
+
+thread1 = threading.Thread(target=rd, args=50)
+thread2 = threading.Thread(target=rd, args=50)
+thread3 = threading.Thread(target=rd, args=50)
